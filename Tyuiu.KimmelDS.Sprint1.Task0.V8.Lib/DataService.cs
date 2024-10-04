e1ba98fa678395cf70fa9540df9589f6713a8db9
EvilHui <Ukrumoski@gmail.com>
2024-10-04 19:24:08 +0500

    title fixed

--- Tyuiu.KimmelDS.Sprint1.Task0.V8.Lib/DataService.cs
+++ Tyuiu.KimmelDS.Sprint1.Task0.V8.Lib/DataService.cs
@@ -4,6 +4,6 @@ namespace Tyuiu.KimmelDS.Sprint1.Task0.V8.Lib
 {
     public class DataService : ISprint1Task0V8
     {
-        public double Calculate() => 15 / 2 / 4;
+        public double Calculate() => 15.0 / 2.0 / 4.0;
     }
 }
