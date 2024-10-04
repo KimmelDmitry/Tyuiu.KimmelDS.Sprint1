0fa57360d233c54164ee33223c76cc997978a86a
EvilHui <Ukrumoski@gmail.com>
2024-10-04 20:08:53 +0500

    task 2

---
+++ Tyuiu.KimmelDS.Sprint1.Task2.V29.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KimmelDS.Sprint1.Task2.V29.Lib
+{
+    public class DataService : ISprint1Task2V29
+    {
+        public int ConvertSecondsToHours(int value)
+        {
+            if (value <= 0)
+            {
+                Console.WriteLine("недопустимое значение");
+                return 0;
+            }
+            return value / 60;
+        }
+    }
+}
---
+++ Tyuiu.KimmelDS.Sprint1.Task2.V29.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.KimmelDS.Sprint1.Task2.V29.Lib;
+
+namespace Tyuiu.KimmelDS.Sprint1.Task2.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]    
+        public void CheckDataServiceValid()
+        {
+            var service = new DataService();
+            Assert.AreEqual(1, service.ConvertSecondsToHours(112));
+        }
+    }
+}
