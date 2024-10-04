5b809824fe3f43c44b1ad6017946dce911185d69
EvilHui <Ukrumoski@gmail.com>
2024-10-04 23:30:02 +0500

    Task 5

---
+++ Tyuiu.KimmelDS.Sprint1.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KimmelDS.Sprint1.Task5.V1.Lib;
+
+namespace Tyuiu.KimmelDS.Sprint1.Task5.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckDistanceBetweenDotsValid()
+        {
+            var ds = new DataService();
+            
+            Assert.AreEqual(1, ds.DistanceBetweenDots(1, 1, 2, 2));
+        }
+    }
+}
